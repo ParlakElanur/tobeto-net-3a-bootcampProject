7c713561f090d3598b6153795754e0204a2719b6
ElanurGuduk <49445642+GudukElanur@users.noreply.github.com>
2024-02-28 12:57:47 +0300

    #10 Added exceptions

---
+++ Core/Exceptions/Types/NotFoundException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Exceptions.Types
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException()
+        {
+        }
+
+        public NotFoundException(string? message) : base(message)
+        {
+        }
+        public NotFoundException(string? message, Exception? exception) : base(message, exception)
+        {
+        } 
+        protected NotFoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
